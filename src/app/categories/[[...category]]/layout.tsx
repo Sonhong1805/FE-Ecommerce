4ac88c48c16d71fa96e11c947b1dac6d5ccda288
Sonhong1805 <nguyenhongson18052003@gmail.com>
2024-06-26 16:52:33 +0700

    add opengraph categories

--- src/app/categories/[[...category]]/layout.tsx
+++ src/app/categories/[[...category]]/layout.tsx
@@ -1,4 +1,5 @@
 import baseURL from "@/constants/baseURL";
+import nextURL from "@/constants/nextURL";
 import type { Metadata } from "next";
 
 type Props = {
@@ -18,6 +19,15 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
       : "Cửa hàng - Mua sắm cùng MONA MEDIA",
     description:
       "khám phá và mua sắm các sản phẩm từ các danh mục khác nhau. Tại đây, bạn có thể tìm thấy một loạt các sản phẩm đa dạng và chất lượng, từ đó đáp ứng nhu cầu mua sắm của mình một cách đầy đủ và thoải mái.",
+    openGraph: {
+      title: categories.objCategory?.name
+        ? categories.objCategory?.name + " - Mua sắm cùng MONA MEDIA"
+        : "Cửa hàng - Mua sắm cùng MONA MEDIA",
+      description:
+        "khám phá và mua sắm các sản phẩm từ các danh mục khác nhau. Tại đây, bạn có thể tìm thấy một loạt các sản phẩm đa dạng và chất lượng, từ đó đáp ứng nhu cầu mua sắm của mình một cách đầy đủ và thoải mái.",
+      type: "website",
+      images: [nextURL + "/images/logo.png"],
+    },
   };
 }
 
