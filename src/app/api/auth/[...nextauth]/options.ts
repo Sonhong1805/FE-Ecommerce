7fa2701ac122875505d6e77a6e78c11d27e2962f
Sonhong1805 <nguyenhongson18052003@gmail.com>
2024-06-26 16:22:22 +0700

    add google key 6

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -5,8 +5,9 @@ import FacebookProvider from "next-auth/providers/facebook";
 export const options: NextAuthOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      clientId:
+        "204435003902-bcnedst7vpreoonm50eo92f500a7q4cr.apps.googleusercontent.com",
+      clientSecret: "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",
     }),
     FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID as string,
